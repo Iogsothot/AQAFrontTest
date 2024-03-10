3e4658bcc5dc029eb5f32eb27a1bb92b120dfc33
Iogsothot <73393896+Iogsothot@users.noreply.github.com>
2024-03-10 18:58:30 +0300

    add allure

--- src/test/java/HMVTest.java
+++ src/test/java/HMVTest.java
@@ -19,7 +19,6 @@ public class HMVTest {
         highlights = new HighlightsConfig_qaPage(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        //driver.get(ConfProperties.getProperty("loginpage"));
         HighlightsConfig_qaPageSteps.logInStep();
     }
 
@@ -28,58 +27,13 @@ public class HMVTest {
         driver.quit();
     }
 
-   // @Test
-    public void loginTest() throws InterruptedException {
-        Thread.sleep(1000l);
-        LoginPage.clickCookiesBtn();
-        LoginPage.inputLogin(ConfProperties.getProperty("login"));
-        LoginPage.inputPasswd(ConfProperties.getProperty("password"));
-        LoginPage.clickLoginBtn();
-    }
 
-   // @Test
-    public void addSportTest1() throws InterruptedException {
-        HighlightsConfig_qaPage.clickSelectLanguage();
-        HighlightsConfig_qaPage.clickAddSportButton();
-        HighlightsConfig_qaPage.clickSelectSportButton();
-        Thread.sleep(100L);
-        HighlightsConfig_qaPage.clickApplySportAdd();
-        HighlightsConfig_qaPage.clickSaveConfig();
-        Thread.sleep(10000L);
-        String sport = highlights.assertTextWhatAddSport();
-        Assertions.assertEquals("Archery", sport);
-        HighlightsConfig_qaPage.clickDisableFourSport();
-        HighlightsConfig_qaPage.clickDeleteDisableFourSport();
-        Thread.sleep(10000L);
-        HighlightsConfig_qaPage.clickSaveConfig();
-    }
      @Test
     public void addSportTest() throws InterruptedException {
         HighlightsConfig_qaPageSteps.addSportStep();
          HighlightsConfig_qaPageSteps.deleteAddSportStep();
      }
-    //@Test
-    public void addEventTest1() throws InterruptedException {
-        Thread.sleep(1000L);
-        HighlightsConfig_qaPage.clickToDate();
-        HighlightsConfig_qaPage.inputToDate(ConfProperties.getProperty("to"));
-        HighlightsConfig_qaPage.clickFromDate();
-        HighlightsConfig_qaPage.inputFromDate(ConfProperties.getProperty("from"));
-        HighlightsConfig_qaPage.inputEvent(ConfProperties.getProperty("team"));
-        HighlightsConfig_qaPage.clickSearchEventButton();
-        Thread.sleep(1000L);
-        HighlightsConfig_qaPage.clickAddEvent();
-        HighlightsConfig_qaPage.clickAddEvent();
-        HighlightsConfig_qaPage.clickAddIsPromo1();
-        HighlightsConfig_qaPage.clickAddIsPromo2();
-        HighlightsConfig_qaPage.clickSaveConfig();
-        Thread.sleep(1000L);
-        HighlightsConfig_qaPage.clickDeleatIsPromo2();
-        Thread.sleep(1000L);
-        HighlightsConfig_qaPage.clickAddIsSafe2();
-        Thread.sleep(1000L);
-        HighlightsConfig_qaPage.clickSaveConfig();
-    }
+
     @Test
     public void addEventTest() throws InterruptedException {
         HighlightsConfig_qaPageSteps.searchEventStep();
@@ -87,41 +41,12 @@ public class HMVTest {
         HighlightsConfig_qaPageSteps.checkTagEventStep();
     }
 
-    //@Test
-    public void deleteEventTest1() throws InterruptedException {
-        HighlightsConfig_qaPage.clickDeleteEvent();
-        HighlightsConfig_qaPage.clickDeleteEvent();
-        Thread.sleep(1000L);
-        HighlightsConfig_qaPage.clickSaveConfig();
-    }
+
     @Test
     public void deleteEventTest() throws InterruptedException {
         HighlightsConfig_qaPageSteps.deleteEventStep();
     }
-    //@Test
-    public void addLanguageCustom1() throws InterruptedException {
-        Thread.sleep(100L);
-        HighlightsConfig_qaPage.clickEditeLanguageCustom();
-        Thread.sleep(100L);
-        HighlightsConfig_qaPage.clickOpenListLanguageCustom();
-        Thread.sleep(100L);
-        HighlightsConfig_qaPage.clickSelectLanguageCustom1();
-        HighlightsConfig_qaPage.clickSelectLanguageCustom2();
-        Thread.sleep(100L);
-        HighlightsConfig_qaPage.clickAddLanguageCustom();
-        Thread.sleep(100L);
-        HighlightsConfig_qaPage.clickSaveLanguageCustom();
-        Thread.sleep(100L);
-        HighlightsConfig_qaPage.clickEditeLanguageCustom();
-        Thread.sleep(100L);
-        HighlightsConfig_qaPage.clickDeleteLanguageCustom();
-        Thread.sleep(100L);
-        HighlightsConfig_qaPage.clickSelectLanguageCustomDelete1();
-        HighlightsConfig_qaPage.clickSelectLanguageCustomDelete2();
-        Thread.sleep(100L);
-        HighlightsConfig_qaPage.clickDeleteButtonLanguageCustom();
-        HighlightsConfig_qaPage.clickSaveLanguageCustom();
-    }
+
     @Test
     public void addLanguageCustom() throws InterruptedException {
         HighlightsConfig_qaPageSteps.addCustomLanguageStep();
